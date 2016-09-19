8fcb1c0e5b14af321a517ba720a1b30c42eeca3a
Robert Ying <robert@dropbox.com>
2016-09-19 10:13:37 -0700

    Removing vim cleanup

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-echo "Removing old vim configuration"
-rm -rf .vim .vimrc
-
 echo "Downloading vim plug"
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
